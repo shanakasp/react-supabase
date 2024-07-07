d285f86f5feaa01887da3f78e1f9afd996637f9b
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-07-07 20:32:20 +0530

    modified Card.jsx and Update.jsx

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Card = ({ smoothie }) => {
   return (
@@ -6,6 +7,11 @@ const Card = ({ smoothie }) => {
       <h3>{smoothie.title}</h3>
       <p>{smoothie.methid}</p>
       <div className="rating">{smoothie.rating}</div>
+      <div className="buttons">
+        <Link to={"/" + smoothie.id}>
+          <i className="material-icons">edit</i>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,7 +1,74 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import supabase from "../config/supaBaseClient";
+
 const Update = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [methid, setMethid] = useState("");
+  const [rating, setRating] = useState("");
+
+  useEffect(() => {
+    fetchSmoothie();
+  }, [id, navigate]);
+
+  const fetchSmoothie = async () => {
+    const { data, error } = await supabase
+      .from("helloworld")
+      .select()
+      .eq("id", id)
+      .single();
+
+    if (error) {
+      console.log(error);
+      navigate("/", { replace: true });
+    }
+    if (data) {
+      setTitle(data.title);
+      setMethid(data.methid);
+      setRating(data.rating);
+      console.log(data);
+    }
+  };
+
   return (
     <div className="page update">
-      <h2>Update</h2>
+      <h2>Update - {id}</h2>
+      <form>
+        <label>
+          <span>Title:</span>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Method:</span>
+          <input
+            type="text"
+            value={methid}
+            onChange={(e) => setMethid(e.target.value)}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Rating:</span>
+          <input
+            type="number"
+            value={rating}
+            onChange={(e) => setRating(e.target.value)}
+            required
+          />
+        </label>
+
+        <button type="submit">Update</button>
+        {/* {formError && <p className="error">{formError}</p>} */}
+      </form>
     </div>
   );
 };
