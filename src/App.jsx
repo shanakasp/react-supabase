61dd6b730c1c16ad3ff4445690395cc442ef2e2d
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-07-07 11:22:20 +0530

    changed 7 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+import Create from "./pages/Create";
+import Home from "./pages/Home";
+import Update from "./pages/Update";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     
-    </>
-  )
+    <BrowserRouter>
+      <nav>
+        <h1>Supa Smoothies</h1>
+        <Link to="/">Home</Link>
+        <Link to="/create">Create New Smoothie</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/:id" element={<Update />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
