9fba1a44afdc3900d06aa1b3e7bbc9d6c6fabc73
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-07-07 13:28:25 +0530

    changed 5 files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import supabase from "../config/supaBaseClient";
+
 const Home = () => {
+  const [fetchError, setFetchError] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const { data, error } = await supabase.from("helloworld").select("*");
+
+    if (error) {
+      setFetchError("Could not fetch the data");
+      setData(null);
+      console.log(error);
+    }
+
+    if (data) {
+      setData(data);
+      console.log(data);
+      setFetchError(null);
+    }
+  };
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && <p>{fetchError}</p>}
+
+      {data && data.map((item) => <p key={item.id}>{item.title}</p>)}
     </div>
   );
 };
