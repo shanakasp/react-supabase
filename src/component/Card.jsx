9514596b5a53b97ee625849dae79756c99a34fc6
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-07-08 09:08:06 +0530

    modified 4 files

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import supabase from "../config/supaBaseClient";
 
 const Card = ({ smoothie }) => {
+  const handleDelete = async () => {
+    const { data, error } = await supabase
+      .from("helloworld")
+      .delete()
+      .eq("id", smoothie.id);
+    if (error) {
+      console.log(error);
+    }
+    if (data) {
+      console.log(data);
+    }
+  };
   return (
     <div className="smoothie-card">
       <h3>{smoothie.title}</h3>
@@ -11,6 +24,9 @@ const Card = ({ smoothie }) => {
         <Link to={"/" + smoothie.id}>
           <i className="material-icons">edit</i>
         </Link>
+        <i className="material-icons" onClick={handleDelete}>
+          delete
+        </i>
       </div>
     </div>
   );
