ae7992deab886f784c4f086255491e6df6d66530
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-07-08 11:32:50 +0530

    modified Home.jsx in pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,17 @@ import supabase from "../config/supaBaseClient";
 const Home = () => {
   const [fetchError, setFetchError] = useState(null);
   const [data, setData] = useState(null);
+  const [orderBy, setOrderBy] = useState("created_at");
 
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    const { data, error } = await supabase.from("helloworld").select("*");
+    const { data, error } = await supabase
+      .from("helloworld")
+      .select()
+      .order(orderBy, { ascending: false });
 
     if (error) {
       setFetchError("Could not fetch the data");
@@ -23,15 +27,28 @@ const Home = () => {
     }
   };
 
+  const handleDelete = (id) => {
+    setData((prevData) => prevData.filter((item) => item.id !== id));
+  };
+
   return (
     <div className="page home">
       {fetchError && <p>{fetchError}</p>}
 
       {data && (
         <div className="smoothies">
+          <div className="order-by">
+            <p>Order by:</p>
+            <button onClick={() => setOrderBy("created_at")}>
+              Time Created
+            </button>
+            <button onClick={() => setOrderBy("title")}> Title</button>
+            <button onClick={() => setOrderBy("Rating")}>Rating</button>
+            {orderBy}
+          </div>
           <div className="smoothie-grid">
             {data.map((item) => (
-              <Card key={item.id} smoothie={item} />
+              <Card key={item.id} smoothie={item} onDelete={handleDelete} />
             ))}
           </div>
         </div>
